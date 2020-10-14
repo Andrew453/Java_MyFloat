cb2152bed4e1c8406154f277a4fdc645136cbe32
Andrew Prokushev <senior.prockuschev2017@yandex.ru>
2020-10-14 16:54:28 +0300

    Added Main for MyFloat

---
+++ Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	MyFloat a = new MyFloat(123320002,3);
+	MyFloat b = new MyFloat(232400003,3);
+	a.sub(b);
+	a.sum(b);
+
+    }
+}
